7f3e28d47288f18f70c21e16af7c12e3469cdf1d
SkinnyAstro <sartapeprasad@gmail.com>
2025-11-13 21:11:40 +0530

    Added a new cart page file

---
+++ src/test/java/pages/CartPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage extends BasePage {
+
+
+
+
+
+
+    public CartPage(WebDriver driver){
+        super(driver);
+
+    }
+
+}
